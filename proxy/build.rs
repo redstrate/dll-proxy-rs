a74f9cd5dd4ec045755acfcd926478300d499310
Joshua Goins <josh@redstrate.com>
2025-04-19 20:07:48 -0400

    Attempt to fix linking under MinGW

--- proxy/build.rs
+++ proxy/build.rs
@@ -15,7 +15,7 @@ fn main() {
     let lib_path = Path::new("deps").join("Exports.def");
     let absolute_path = std::fs::canonicalize(&lib_path).unwrap();
     println!(
-        "cargo:rustc-cdylib-link-arg=/DEF:{}",
+        "cargo:rustc-cdylib-link-arg={}",
         absolute_path.display()
     );
 }
